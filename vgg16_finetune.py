a2b1a688b3819e15110c21d3cded6b2b24beedd4
jinjieliu <jinjie.liu@usc.edu>
2025-04-22 17:25:05 -0700

    rewrite requirements.txt and script
    Signed-off-by: jinjieliu <jinjie.liu@usc.edu>

--- vgg16_finetune.py
+++ vgg16_finetune.py
@@ -10,35 +10,7 @@ from torchvision import datasets, transforms, models
 import flag_gems
 flag_gems.enable()  # Enable FlagGems for accelerated Triton-backed kernels :contentReference[oaicite:2]{index=2}
 
-# def get_dataloaders(data_dir, batch_size, num_workers=4):
-#     """Prepare ImageNet-style training and validation dataloaders."""
-#     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-#                                      std=[0.229, 0.224, 0.225])
-#     train_transform = transforms.Compose([
-#         transforms.RandomResizedCrop(224),
-#         transforms.RandomHorizontalFlip(),
-#         transforms.ToTensor(),
-#         normalize,
-#     ])
-#     val_transform = transforms.Compose([
-#         transforms.Resize(256),
-#         transforms.CenterCrop(224),
-#         transforms.ToTensor(),
-#         normalize,
-#     ])
-#     train_ds = datasets.ImageFolder(os.path.join(data_dir, 'train'),
-#                                    train_transform)
-#     val_ds   = datasets.ImageFolder(os.path.join(data_dir, 'val'),
-#                                    val_transform)
-#     train_loader = torch.utils.data.DataLoader(
-#         train_ds, batch_size=batch_size, shuffle=True,
-#         num_workers=num_workers, pin_memory=True)
-#     val_loader = torch.utils.data.DataLoader(
-#         val_ds, batch_size=batch_size, shuffle=False,
-#         num_workers=num_workers, pin_memory=True)
-#     return train_loader, val_loader
-
-def get_dataloaders(batch_size=4, num_workers=2):
+def get_dataloaders(batch_size=4, num_workers=os.cpu_count()):
     mean_values = (0.5, 0.5, 0.5)
     std_values = (0.5, 0.5, 0.5)
 
