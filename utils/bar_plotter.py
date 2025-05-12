4b2e6516c9f439183533e317552f12ec10811818
Warso30 <w2280077822@gmail.com>
2025-05-11 23:15:37 -0700

    Add more scripts for visualization

---
+++ utils/bar_plotter.py
@@ -0,0 +1,55 @@
+import json
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import argparse
+from pathlib import Path
+
+
+def calculate_total_time(stats):
+    total_time = 0
+    for stat in stats:
+        total_time += stat["duration"]
+    return total_time
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--data-dir",
+    type=str,
+    required=True,
+    help="The directory that contains the json data files.",
+)
+args = parser.parse_args()
+data_dir = Path(args.data_dir)
+
+records = []
+for json_file in data_dir.glob("*.json"):
+    model_name, type_name, _ = json_file.stem.split("_", 2)
+    with open(json_file, "r") as f:
+        data = json.load(f)
+    total_time = calculate_total_time(data["round_stats"])
+    records.append(
+        {"model_name": model_name, "type_name": type_name, "total_time": total_time}
+    )
+df = pd.DataFrame(records)
+
+default_total_times = df[df.type_name == "default"].set_index("model_name")[
+    "total_time"
+]
+df["scaled_time"] = df.apply(
+    lambda r: r["total_time"] / default_total_times[r["model_name"]], axis=1
+)
+
+plt.figure(figsize=(12, 5))
+sns.set_theme(style="darkgrid")
+sns.barplot(
+    data=df, x="model_name", y="scaled_time", hue="type_name", palette="dark", alpha=0.8
+)
+plt.xticks(rotation=45, ha="right")
+plt.ylabel("Ratio to Default Autotuner")
+plt.xlabel("Model")
+plt.title("End-to-End Training Time on Different Autotuner Modes")
+plt.legend(title="Autotuner Mode")
+# plt.tight_layout()
+plt.show()
---
+++ utils/infer_plotter.py
@@ -0,0 +1,66 @@
+import json
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import argparse
+
+
+def preprocess(type_name, json_path):
+    with open(json_path, "r") as f:
+        stats = json.load(f)
+    tokens_per_sec = []
+    total_time = 0
+    for num_tokens, s in enumerate(stats["round_stats"]):
+        total_time += s["duration"]
+        # tokens_per_sec.append(1000 * (num_tokens + 1) / total_time)
+        tokens_per_sec.append(1000 / s["duration"])
+    stat = pd.DataFrame(
+        {
+            "num_tokens": np.arange(1, len(tokens_per_sec) + 1),
+            "tokens_per_sec": tokens_per_sec,
+            "type_name": type_name,
+        }
+    )
+    return stat
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--default",
+    type=str,
+    help="JSON file containing stats for inference with DefaultAutotuner",
+)
+parser.add_argument(
+    "--epsilon",
+    type=str,
+    help="JSON file containing stats for inference with EpsilonAutotuner",
+)
+parser.add_argument(
+    "--stepwise",
+    type=str,
+    help="JSON file containing stats for inference with StepwiseAutotuner",
+)
+args = parser.parse_args()
+
+df = pd.concat(
+    [
+        preprocess("default", args.default),
+        preprocess("epsilon", args.epsilon),
+        preprocess("stepwise", args.stepwise),
+    ],
+    ignore_index=True,
+)
+plt.figure(figsize=(8, 5))
+sns.scatterplot(
+    data=df,
+    x="num_tokens",
+    y="tokens_per_sec",
+    hue="type_name",
+)
+plt.xlabel("Number of Generated Tokens")
+plt.ylabel("Tokens per Second")
+plt.title("Llama Inference Speed on Different Autotuner Modes")
+plt.legend(title="Autotuner Mode")
+plt.tight_layout()
+plt.show()
